5e8e963add86505bbd05b33d7102fd96aeb2739b
sagar5412 <sagar.gk.bly@gmail.com>
2025-11-22 00:26:18 +0530

    learned lodash.get, async function error handler

--- Express server/route.js
+++ Express server/route.js
@@ -1,30 +1,62 @@
 import express from "express";
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  // fetch inputs from body
+// async function error handler - real world uses it.
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
-  // fetch data from db
+// lodash.get - For safe property access returns null instead of undefined helps to prevent server crash
 
-  return res.json({
-    msg: "get request",
-  });
+function get(obj, path, defaultValue) {
+  const keys = path.split(".");
+  let result = obj;
+  for (const key of keys) {
+    result = result?.[key];
+    if (result === undefined) return defaultValue;
+  }
+  return result;
+}
+
+app.get(
+  "/users",
+  asyncHandler(async (req, res) => {
+    // async db call
+    throw new Error();
+  })
+);
+
+app.get(
+  "/",
+  asyncHandler(async(req, res) => {
+    // fetch inputs from body
+    const username = get(req, "body.username", null);
+    const result = await db.user.create({});
+    // fetch data from db
+
+    return res.json({
+      msg: "get request",
+      username
+    });
+  })
+);
+
+app.post(
+  "/users",
+  asyncHandler((req, res) => {
+    // fetch inputs from body
+
+    // put data into db
+
+    return res.json({
+      msg: "user created successfully",
+    });
+  })
+);
+
+app.get("/error", (req, res) => {
+  throw new Error();
 });
-
-app.post("/users", (req, res) => {
-  // fetch inputs from body
-
-  // put data into db
-
-  return res.json({
-    msg: "user created successfully",
-  });
-});
-
-app.get("/error",(req,res)=>{
-    throw new Error;
-})
 
 app.use((req, res, next) => {
   res.status(404).json({
@@ -33,9 +65,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  return res.json({
-    msg:"something wrong"
-  });
+  res.status(500).json({ msg: "Something went wrong" });
 });
 
-app.listen(3000)
+app.listen(3000);
